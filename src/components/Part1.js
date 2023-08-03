1ccf7c0c419b0f4f68d6c46253b7b8787bb7a468
ImChopi <apchopito@gmail.com>
2023-08-03 20:31:07 +0100

    FIxed components/Part1 (Props part1, exercises1)

--- src/components/Part1.js
+++ src/components/Part1.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-// Forma 2
+// Forma
 const Part1 = (props) => {
     const {part1, exercises1} = props;
-    return <p> {props.part1} {props.exercises1} </p>
+    return <p> {part1} {exercises1} </p>
 }
 
 export default Part1
