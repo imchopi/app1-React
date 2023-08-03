66b6191383f5c280fbb0a2dfcba34cd2a3ad212b
ImChopi <apchopito@gmail.com>
2023-08-03 20:30:33 +0100

    Fixed props (part1, exercises1)

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
+/* Forma 1
+  const Title = (props) => {
+    const course = props.course --> Forma 1
+    return <h1>{course}</h1>
+  }
+*/
+
+/* Forma 2
+  const Title = (props) => {
+    const {course} = props --> Forma 2
+    return <h1>{course}</h1>
+  }
+*/
+
+/* Forma 3
+  const Title = ({course}) => {
+    return <h1>{course}</h1>
+  }
+*/
+
 // Forma 4 (One liner)
 const Title = ({course}) => <h1>{course}</h1>
 
